71804868dbf5d3516b7a759b8c1f93ec9f3312a8
EarningScripter <90612386+EarningScripter@users.noreply.github.com>
2022-07-30 07:34:09 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 function getNumber(){
 var numberBox = document.getElementById("number");
 /****************Click here to increase the limit on random numbers***************/
-var number = Math.floor(Math.random()* 9 + 1);
+var number = Math.floor(Math.random()* 10 + 1);
 numberBox.innerHTML = number;
 }
